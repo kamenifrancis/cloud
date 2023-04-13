e20146ff7c9c4015f9daea262fb37cf00a0309ad
kamenifrancis <blickson57@gmail.com>
2023-04-13 15:32:31 +0100

    checkpoint de cloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MapContainer extends Component {
+  render() {
+    const mapStyles = {
+      width: '100%',
+      height: '100%'
+    };
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 37.7749,
+         lng: -122.4194
+        }}
+      />
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'YOUR_API_KEY_GOES_HERE'
+})(MapContainer);
+
